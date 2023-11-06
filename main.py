1b19f9ad1a1982df826de7e3d46f689bd47116f1
omar mohamed <omarmohhameed828@gmail.com>
2023-11-06 16:37:16 +0200

    plot initial time, frequency spectrum graphs

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from threading import Thread
 import numpy as np
 import math
 from PyQt6.QtCore import QTimer
-
+from scipy.signal import spectrogram
 
 from helpers.get_signal_from_file import get_signal_from_file
 from models.signal import Signal
@@ -25,7 +25,7 @@ class MainWindow(uiclass, baseclass):
         self._initialize_signals_slots()
         self.signal = None
         self.playing = False
-        self.last_index = 0 
+        self.last_index = 0
         self.current_timer = QTimer()
         self.current_timer.timeout.connect(self.update_current_timer)
         self.current_time = 0
@@ -37,55 +37,92 @@ class MainWindow(uiclass, baseclass):
 
     def _on_input_slider_change(self, value):
         if self.signal:
-            self.current_time = value /1000
+            self.current_time = value / 1000
             self.update_current_timer()
-            
-    
+
     def _import_signal_file(self):
         self.signal: Signal = get_signal_from_file(self)
+
+        # plot time graph
         pen_c = pg.mkPen(color=(255, 255, 255))
         self.input_signal_graph.plot(self.signal.x_vec, self.signal.y_vec, pen=pen_c)
+
+        # plot initial output time graph
+        self.output_signal_graph.plot(self.signal.x_vec, self.signal.y_vec, pen=pen_c)
+
         self.input_slider.setMinimum(0)
         self.input_slider.setMaximum(int(self.signal.x_vec[-1] * 1000))
         self.input_slider.setValue(0)
-        self.input_total_time.setText(f'{str(math.floor(self.signal.x_vec[-1] / 60)).zfill(2)}:{str(math.floor(self.signal.x_vec[-1]) % 60).zfill(2)}')  
+        self.input_total_time.setText(
+            f'{str(math.floor(self.signal.x_vec[-1] / 60)).zfill(2)}:{str(math.floor(self.signal.x_vec[-1]) % 60).zfill(2)}')
+
+        # plot input spectrogram
+        self.plot_input_spectrogram()
+
+    def plot_input_spectrogram(self):
+        if self.signal.audio:
+            sampling_frequency = self.signal.audio.frame_rate
+        else:
+            sampling_frequency = 1000
+        # Frequency domain representation
+        amplitude = self.signal.y_vec
+        fourier_transform = np.fft.fft(amplitude) / len(amplitude)  # Normalize amplitude
+
+        fourier_transform = fourier_transform[range(int(len(amplitude) / 2))]  # Exclude sampling frequency
+
+        tp_count = len(amplitude)
+
+        values = np.arange(int(tp_count / 2))
+
+        time_period = tp_count / sampling_frequency
+
+        frequencies = values / time_period
+        pen_c = pg.mkPen(color=(255, 255, 255))
+        self.input_spectrogram_graph.plot(frequencies, abs(fourier_transform), pen=pen_c)
+
+        # plot initial output spectrogram
+        self.output_spectrogram_graph.plot(frequencies, abs(fourier_transform), pen=pen_c)
+
+
+
+
 
     def play_audio(self):
         if self.signal.audio:
             self.playing = True
             final_index = np.abs(self.signal.x_vec - self.current_time).argmin()
-            sd.play(self.signal.y_vec[final_index:], self.signal.audio.frame_rate *2)
+            sd.play(self.signal.y_vec[final_index:], self.signal.audio.frame_rate * 2)
             sd.wait()
             self.current_timer.stop()
             self.playing = False
             final_index = np.abs(self.signal.x_vec - self.current_time).argmin()
-            if(final_index >= len(self.signal.x_vec)-100):
+            if (final_index >= len(self.signal.x_vec) - 100):
                 self.input_play_button.setText('Rewind')
                 self.current_time = 0;
-                
 
     def play_time_signal(self):
-       if self.signal.audio:
+        if self.signal.audio:
             if self.playing:
                 sd.stop()
                 self.playing = False
                 self.input_play_button.setText('Play')
                 self.current_timer.stop()
-                
+
             else:
                 self.audio_thread = Thread(target=self.play_audio)
                 self.audio_thread.start()
                 self.current_timer.start(10)
-                self.playing = True     
+                self.playing = True
                 self.input_play_button.setText('Pause')
 
     def update_current_timer(self):
-        self.current_time+=0.01;
-        self.current_input_time.setText(f'{str(math.floor(self.current_time / 60)).zfill(2)}:{str(math.floor(self.current_time) % 60).zfill(2)}')  
+        self.current_time += 0.01;
+        self.current_input_time.setText(
+            f'{str(math.floor(self.current_time / 60)).zfill(2)}:{str(math.floor(self.current_time) % 60).zfill(2)}')
         self.input_slider.blockSignals(True)
         self.input_slider.setValue(math.ceil(self.current_time * 1000))
         self.input_slider.blockSignals(False)
-        self.input_slider.repaint()   
+        self.input_slider.repaint()
 
 
 def main():
--- models/signal.py
+++ models/signal.py
@@ -1,12 +1,26 @@
 from enum import Enum
+import numpy as np
 
 class SignalType(Enum):
     CONTINUOUS = 0
     DISCRETE = 1
 
+
 class Signal:
-    def __init__(self, x_vec, y_vec, audio= None, signal_type: SignalType = SignalType.CONTINUOUS) -> None:
+    def __init__(self, x_vec, y_vec, audio=None, signal_type: SignalType = SignalType.CONTINUOUS) -> None:
         self.x_vec = x_vec
         self.y_vec = y_vec
         self.audio = audio
         self.signal_type = signal_type
+
+    def get_sampling_frequency(self):
+        if self.audio:
+            sampling_frequency = self.audio.frame_rate
+        else:
+            time_difference = np.diff(self.x_vec)
+            average_time_difference = np.mean(time_difference)
+            sampling_frequency = 1 / average_time_difference
+
+        return sampling_frequency
+
+
