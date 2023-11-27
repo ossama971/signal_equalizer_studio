efe8ca83105a2990ec2c3be9ac038139e208acd3
omar mohamed <omarmohhameed828@gmail.com>
2023-11-27 08:10:44 +0200

    spectrogram demo 2

--- main.py
+++ main.py
@@ -69,21 +69,39 @@ class MainWindow(uiclass, baseclass):
         self.frequency_graph.plot(frequencies, abs(fourier_transform), pen=pen_c)
 
     def plot_input_spectrograph(self):
-        # # Compute the spectrogram using scipy's spectrogram function
-        # amplitude = self.signal.y_vec
-        # sampling_rate = self.signal.get_sampling_frequency()
-        # f, t, Sxx = spectrogram(amplitude, fs=sampling_rate)
-        #
-        # # Plot the spectrogram
-        # np.seterr(divide='ignore')
-        # plt.pcolormesh(t, f, 10 * np.log10(Sxx), shading='auto')
-        # plt.title('Spectrograph')
-        # plt.xlabel('time')
-        # plt.ylabel('frequency')
-        # plt.colorbar(label='Intensity (dB)')  # Add colorbar
-        #
-        # # Show the plot
-        # # plt.show()
+        # Compute the spectrogram using scipy's spectrogram function
+        amplitude = self.signal.y_vec
+        sampling_rate = self.signal.get_sampling_frequency()
+        f, t, Sxx = spectrogram(amplitude, fs=sampling_rate)
+
+        # Plot the spectrogram
+        np.seterr(divide='ignore')
+        plt.pcolormesh(t, f, 10 * np.log10(Sxx), shading='auto')
+        plt.title('Spectrograph')
+        plt.xlabel('time')
+        plt.ylabel('frequency')
+        plt.colorbar(label='Intensity (dB)')  # Add colorbar
+
+        # Show the plot
+        plt.show()
+        self.input_spectrogram_graph.clear()
+
+        # Create an ImageItem to display the spectrogram
+        spectrogram_image = pg.ImageItem()
+        self.input_spectrogram_graph.addItem(spectrogram_image)
+
+        # Set the spectrogram data and scaling
+        spectrogram_image.setImage(Sxx.T, autoLevels=True, autoDownsample=True)
+
+        # Set custom color map (adjust colors as needed)
+        colormap = pg.ColorMap([0, 0.5, 1], [(0, 0, 255), (0, 255, 0), (0, 255, 0)])
+        spectrogram_image.setColorMap(colormap)
+
+        # Set labels for the axes
+        self.input_spectrogram_graph.setLabel('left', 'Frequency', units='Hz')
+        self.input_spectrogram_graph.setLabel('bottom', 'Time', units='s')
+
+
         # # Clear any previous plots from the PlotWidget
         # self.input_spectrogram_graph.clear()
         #
@@ -104,34 +122,44 @@ class MainWindow(uiclass, baseclass):
         # self.input_spectrogram_graph.setLabel('bottom', 'Time', units='s')
         #
 
-        amplitude = self.signal.y_vec
-        sampling_rate = self.signal.get_sampling_frequency()
-        f, t, Sxx = spectrogram(amplitude, fs=sampling_rate)
 
-        np.seterr(divide='ignore')
 
-        # Plot using Matplotlib
-        plt.pcolormesh(t, f, 10 * np.log10(Sxx), shading='auto')
-        plt.title('Spectrogram')
-        plt.xlabel('Time')
-        plt.ylabel('Frequency')
-        plt.colorbar(label='Intensity (dB)')
 
-        # Clear previous plots from the PyQtGraph PlotWidget
-        self.input_spectrogram_graph.clear()
-
-        # Create an ImageItem to display the spectrogram
-        spectrogram_image = pg.ImageItem()
-        self.input_spectrogram_graph.addItem(spectrogram_image)
-
-        # Set the spectrogram data and scaling
-        spectrogram_image.setImage(Sxx)
+        #
+        # amplitude = self.signal.y_vec
+        # sampling_rate = self.signal.get_sampling_frequency()
+        # f, t, Sxx = spectrogram(amplitude, fs=sampling_rate)
+        #
+        # np.seterr(divide='ignore')
+        #
+        # # Plot using Matplotlib
+        # plt.pcolormesh(t, f, 10 * np.log10(Sxx), shading='auto')
+        # plt.title('Spectrogram')
+        # plt.xlabel('Time')
+        # plt.ylabel('Frequency')
+        # plt.colorbar(label='Intensity (dB)')
+        #
+        # # Clear previous plots from the PyQtGraph PlotWidget
+        # self.input_spectrogram_graph.clear()
+        #
+        # # Create an ImageItem to display the spectrogram
+        # spectrogram_image = pg.ImageItem()
+        # self.input_spectrogram_graph.addItem(spectrogram_image)
+        #
+        # # Set the spectrogram data and scaling
+        # spectrogram_image.setImage(Sxx)
+        #
+        # # Set aspect ratio
         # spectrogram_image.setAspectLocked()
-        # spectrogram_image.setScale((t[-1] - t[0]) / len(t), (f[-1] - f[0]) / len(f))
+        #
+        # # Set custom color map (adjust colors as needed)
+        # colormap = pg.ColorMap([0, 0.5, 1], [(0, 0, 0), (255, 20, 99), (82, 4, 2)])
+        # spectrogram_image.setColorMap(colormap)
+        #
+        # # Set labels for the axes
+        # self.input_spectrogram_graph.setLabel('left', 'Frequency', units='Hz')
+        # self.input_spectrogram_graph.setLabel('bottom', 'Time', units='s')
 
-        # Set labels for the axes
-        self.input_spectrogram_graph.setLabel('left', 'Frequency', units='Hz')
-        self.input_spectrogram_graph.setLabel('bottom', 'Time', units='s')
     def apply_fourier_transform(self):
         if self.signal.audio:
             sampling_frequency = self.signal.audio.frame_rate
